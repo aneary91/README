74e30522caa060980d83e1114fd29323722bc350
AllisonWonderland <aneary91@gmail.com>
2021-04-20 18:53:47 -0500

    added questions to the readme and got them working and showing up on the document

--- Develop/index.js
+++ Develop/index.js
@@ -1,40 +1,66 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const util = require('util');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const util = require("util");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 
-        inquirer
-        .prompt([
-          {
-            type: 'input',
-            message: "What is your name?",
-            name: 'username',
-          },
-          {
-            type: 'input',
-            message: "What is your name?",
-            name: 'username2',
-          }
-        ])
-        .then(answers => {
-           writeToFile("README.md", answers);
-          })     
-
-      
-    
-
+var questions = [
+  {
+    type: "input",
+    message: "What is the title of your repository?",
+    name: "title",
+  },
+  //   objects to make: title, description, s table of contents, Installation, Usage, License,Contribution, Tests and QUestions,
+  {
+    type: "input",
+    message: "Describe your project here.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "How do yu use this repository?",
+    name: "usage",
+  },
+  {
+    type: "list",
+    message: "Which license would you like to use?",
+    name: "license",
+    choices: ["MIT", "GPLv2", "Apache", "None"],
+  },
+  {
+    type: "list",
+    message: "What are the guildlines for contributing to this repository?",
+    name: "contribution",
+  },
+  {
+    type: "list",
+    message: "How do you run tests on this repository?",
+    name: "tests",
+    default: "npm test",
+  },
+  {
+    type: "list",
+    message: "What is your github username?",
+    name: "github",
+  },
+  {
+    type: "list",
+    message: "What is your github email?",
+    name: "email",
+  },
+];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-
+  fs.writeFileSync(fileName, data);
 }
-
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    writeToFile("README.md", generateMarkdown({ ...answers }));
+  });
+}
 
 // Function call to initialize app
 init();
