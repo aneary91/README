7e01ab8f2ed75812d25166726a2fedadfc8f145b
AllisonWonderland <aneary@gmail.com>
2021-05-25 17:43:59 -0500

    rendered the read me section and returned an empty string

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -9,11 +9,21 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [license] (#license)\n`
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+if (license !== 'None') {
+  return `## License`;
+}
+return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
