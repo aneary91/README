1e4853084165e88eb360a00eded0437b8fdaa32f
AllisonWonderland <aneary@gmail.com>
2021-05-25 18:30:26 -0500

    added table of contents, and linked github account

--- Develop/README.md
+++
@@ -1,4 +0,0 @@
-{
-	"username": "aljfskjf",
-	"username2": "asldfh'"
-}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license !== 'None') {
+  if (license !== "None") {
     return `![license] (https://img.shields.io/badge/license-${license}-violet.svg)`;
   }
   return "";
@@ -10,8 +10,8 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license !== 'None') {
-    return `\n* [license] (#license)\n`
+  if (license !== "None") {
+    return `\n* [license] (#license)\n`;
   }
   return "";
 }
@@ -19,15 +19,63 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-if (license !== 'None') {
-  return `## License`;
-}
-return "";
+  if (license !== "None") {
+    return `## License`;
+  }
+  return "";
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Usage](#usage)
+    ${renderLicenseLink(data.license)}
+
+  * [Installation](#installation)
+
+
+  * [Contribution](#contribution)
+
+
+  * [Test](#test)
+
+
+  * [Questions](#questions)
+
+
+  ## Usage 
+
+    ${data.usage}
+  
+    ${renderLicenseSection(data.license)}
+
+  ## Installation 
+
+    ${data.installation}
+
+  ## Contribution
+  
+    ${data.contribution}
+
+  ## Test
+
+  ${data.test}
+
+  ## Questions
+
+ If you have any wuestions about this repository, email me @ ${data.email}
+If you woudl like to see my work  @ [${data.github}](https://github.com/${data.github
+    }) 
+
 
 `;
 }
